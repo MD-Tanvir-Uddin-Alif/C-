1ec8181f9554097eb370f67690358d003edff0a1
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-08-08 19:37:08 +0600

    Remove Job Tracker folder from tracking

--- Job Tracker/backend/JobTrackerApi/Dtos/CompanyUpdateDto.cs
+++
@@ -1,7 +0,0 @@
-public class CompanyUpdateDto
-{
-    public string Name { get; set; } = null!;
-    public string? Website { get; set; }
-    public string? Industry { get; set; }
-    public string? Location { get; set; }
-}
--- Job Tracker/backend/JobTrackerApi/Dtos/UserDto.cs
+++
@@ -1,7 +0,0 @@
-public class UserDto
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public DateTime CreatedAt { get; set; }
-}
